0d070811bc97b07d7dbb8a648155a4d450b882e2
unknown <sithumgeeganage125@gmail.com>
2024-05-09 14:51:00 +0530

    Add HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import NavBar from './components/NavBar'
 import DetailsPage from './Pages/DetailsPage'
+import HomePage from './Pages/HomePage'
 function App() {
   return (
     <div>
      <NavBar/>
     <DetailsPage/>
+    <HomePage/>
     </div>
   )
 }
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './HomePage.css'
 
 function HomePage() {
   return (
     <div>
-       
+      <h1 className='title'>The Horoscope</h1> 
+      <div>
+      <img src="RashiChakraya.png" alt="RashiChakraya" className='RashiChakraya'/>
+      </div>
+      {/* <div className='text'>
+        <h2>Swasthi Siddham</h2>
+        <p>smaple Text</p>
+      </div> */}
       
     </div>
   )
