3ac5a51a12bddbab6c1ec866d0790bbd0309c33e
unknown <sithumgeeganage125@gmail.com>
2024-05-11 02:26:35 +0530

    Create a Customer feedback page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import DetailsPage from './Pages/DetailsPage'
 import HomePage from './Pages/HomePage'
+import CustomerReviews from './Pages/CustomerReviews'
 function App() {
   return (
     <div>
-     <NavBar/>
-    <DetailsPage/>
-    <HomePage/>
+      <NavBar/>
+    <CustomerReviews/>
+
+   
     </div>
   )
 }
---
+++ client/src/Pages/CustomerReviews.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './CustomerReviews.css'
+
+function CustomerReviews() {
+    
+  return (
+    <div className>
+      <h1 className='Title'>Customer Reviews</h1>
+    <div className='customerReviewsContainer'>
+   
+      <div className='customerCard'>
+        <img src="RashiChakraya.png" alt="RashiChakraya" className='cardImage'>
+        </img>
+        <h2>Customer Name</h2>
+        <p>That was a amazing job. Thank you very much </p>
+      </div>
+      <div className='customerCard'>
+        <img src="RashiChakraya.png" alt="RashiChakraya" className='cardImage'>
+        </img>
+        <h2>Customer Name</h2>
+        <p>That was a amazing job. Thank you very much </p>
+      </div>
+      <div className='customerCard'>
+        <img src="RashiChakraya.png" alt="RashiChakraya" className='cardImage'>
+        </img>
+        <h2>Customer Name</h2>
+        <p>That was a amazing job. Thank you very much </p>
+      </div>
+      <div className='customerCard'>
+        <img src="RashiChakraya.png" alt="RashiChakraya" className='cardImage'>
+        </img>
+        <h2>Customer Name</h2>
+        <p>That was a amazing job. Thank you very much </p>
+      </div>
+      <div className='customerCard'>
+        <img src="RashiChakraya.png" alt="RashiChakraya" className='cardImage'>
+        </img>
+        <h2>Customer Name</h2>
+        <p>That was a amazing job. Thank you very much </p>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default CustomerReviews
