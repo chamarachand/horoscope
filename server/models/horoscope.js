67adcaeb996f8095de06d84fd9305a0d1ff1661c
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-05-20 09:08:45 +0530

    Create birthDateTime field instead of birthDay and birthTime

--- server/models/horoscope.js
+++ server/models/horoscope.js
@@ -5,8 +5,7 @@ const horoscopeSchema = new mongoose.Schema({
   name: { type: String, minlength: 5, maxlength: 255, required: true },
   sign: { type: String, minlength: 3, maxlength: 11, required: true },
   gender: { type: String, enum: ["male", "female"] },
-  birthDay: { type: String, minlength: 10, maxlength: 10, required: true },
-  birthTime: { type: String, maxlength: 6, required: true },
+  birthDateTime: { type: Date, required: true },
   birthDistrict: { type: String, maxlength: 25, required: true },
   submitDate: { type: Date, default: Date.now },
 });
@@ -18,8 +17,7 @@ function validateHoroscope(horoscope) {
     name: Joi.string().min(5).max(255).required(),
     sign: Joi.string().min(3).max(11).required(),
     gender: Joi.string().valid("male", "female"),
-    birthDay: Joi.string().length(10),
-    birthTime: Joi.string().max(6).required(),
+    birthDateTime: Joi.date().iso().required(),
     birthDistrict: Joi.string().min(3).max(25).required(),
   });
   return schema.validate(horoscope);
