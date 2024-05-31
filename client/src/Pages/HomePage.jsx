082b799f650425d5827380ef4ad913a7dcf3084d
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-05-31 14:30:45 +0530

    Change the styling of welcome text

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import './HomePage.css'
+import React from "react";
+import "./HomePage.css";
 
 function HomePage() {
   return (
     <div>
-      <div className='HomePage'>
-        <img src="HomePageImage.png" alt="HomePageImage" className='HomePageImage'/>
-        <h1 className='text'>Welcome To The Nakatha Horoscope</h1>
-        <div className='HomePageContent'>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima dignissimos repellat possimus, ratione accusamus placeat blanditiis autem ea debitis natus, distinctio adipisci voluptas, dolorum perspiciatis quod nesciunt suscipit laborum a.</p>
+      <div className="HomePage">
+        <img
+          src="HomePageImage.png"
+          alt="HomePageImage"
+          className="HomePageImage"
+        />
+        <h1 className="text">Welcome To The Nakatha Horoscope</h1>
+        <div className="HomePageContent">
+          <p>
+            Welcome to Celestial Insights, your go-to destination for daily
+            horoscopes and astrological guidance! Whether you're a curious
+            Capricorn, a passionate Pisces, or anywhere in between, our expert
+            astrologers are here to illuminate your path with personalized
+            predictions and cosmic wisdom. Dive into the stars and uncover what
+            the universe has in store for you today!
+          </p>
         </div>
-        
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
