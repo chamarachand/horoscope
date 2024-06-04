0feec503b167333dec68aa2cf169dfb95fb00f2b
unknown <sithumgeeganage125@gmail.com>
2024-06-04 21:45:38 +0530

    Change some CSS in HomePage

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -3,25 +3,17 @@ import "./HomePage.css";
 
 function HomePage() {
   return (
-    <div>
-      <div className="HomePage">
-        <img
-          src="HomePageImage.png"
-          alt="HomePageImage"
-          className="HomePageImage"
-        />
-        <h1 className="text">Welcome To The Nakatha Horoscope</h1>
-        <div className="HomePageContent">
-          <p>
-            Welcome to Celestial Insights, your go-to destination for daily
-            horoscopes and astrological guidance! Whether you're a curious
-            Capricorn, a passionate Pisces, or anywhere in between, our expert
-            astrologers are here to illuminate your path with personalized
-            predictions and cosmic wisdom. Dive into the stars and uncover what
-            the universe has in store for you today!
-          </p>
-        </div>
-      </div>
+    <div id="wrapper">
+      <div className="container-home">
+        <h1>Welcome To The Nakatha Horoscope</h1>
+         </div>
+         <div className="content-home">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+             Laudantium, facilis! Laboriosam commodi dolorem iure quidem! Nisi saepe quibusdam,
+              distinctio rem voluptate libero quisquam sequi possimus labore. 
+              Nesciunt totam architecto minus!</p>
+         </div>
+    
     </div>
   );
 }
