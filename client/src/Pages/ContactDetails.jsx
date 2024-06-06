8922527e7b2cec103004152d16bf0afceda30204
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-06-06 21:50:45 +0530

    Update contact details page

--- client/src/Pages/ContactDetails.jsx
+++ client/src/Pages/ContactDetails.jsx
@@ -9,34 +9,58 @@ export const ContactDetails = () => {
     reset,
   } = useForm();
 
-  const onSubmit = () => {};
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="content">
           <h1>Enter your contact details</h1>
+
           <div className="user-input">
             <label htmlFor="phone-no">
               Telephone Number (ජංගම දුරකතන අංකය)
             </label>
-            <input type="tel" id="phone-no" placeholder="Phone no" />
+            <input
+              type="tel"
+              id="phone-no"
+              placeholder="Phone no"
+              {...register("phoneNo", { required: "Phone number is required" })}
+            />
+            <p className="error">{errors.phoneNo?.message}</p>
           </div>
 
           <div className="user-input">
             <label htmlFor="whatsapp-number">
               Whatsapp number (වට්ස්ඇප් අංකය )
             </label>
-            <input type="tel" id="whatsapp-number" placeholder="Whatsapp no" />
+            <input
+              type="tel"
+              id="whatsapp-number"
+              placeholder="Whatsapp no"
+              {...register("whatsappNo", {
+                required: "Whatsapp number is required",
+              })}
+            />
+            <p className="error">{errors.whatsappNo?.message}</p>
           </div>
 
           <div className="user-input">
             <label htmlFor="email">Email (විද්‍යුත් තැපැල් ලිපිනය )</label>
-            <input type="email" id="email" placeholder="Email" />
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              {...register("email", { required: "Email is required" })}
+            />
+            <p className="error">{errors.email?.message}</p>
           </div>
+
           <div className="buttons">
-            <button>Back</button>
-            <button>Continue</button>
+            <button type="button">Back</button>
+            <button type="submit">Continue</button>
           </div>
         </div>
       </form>
