df98e286c8296df46944c6f16853f69aadfe7910
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-06-16 09:54:35 +0530

    [Client] Made some improvements to the add review component

--- client/src/Pages/AddReview.jsx
+++ client/src/Pages/AddReview.jsx
@@ -4,34 +4,40 @@ import { FaStar } from "react-icons/fa";
 
 export const AddReview = ({ toggleAddReview }) => {
   const [rating, setRating] = useState(0);
-  const [ratingColor, setRatingColor] = useState(null);
+  const [review, setReview] = useState(null);
 
   return (
     <div className="add-review-container">
       <div className="add-review-content">
-        <p>Hello</p>
-        <div>
+        <h1>Add your review</h1>
+        <div className="rating-content">
+          <p>Rating</p>
           {[...Array(5)].map((star, index) => {
             const currentRating = index + 1;
 
             return (
-              <label htmlFor="">
-                <input
-                  type="radio"
-                  name="rate"
-                  value={currentRating}
-                  onClick={() => setRatingColor(currentRating)}
-                />
-                <FaStar
-                  size={50}
-                  color={
-                    currentRating <= (ratingColor || rating) ? "yellow" : "grey"
-                  }
-                />
-              </label>
+              <FaStar
+                key={index}
+                size={25}
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setRating(
+                    currentRating === rating ? rating - 1 : currentRating
+                  );
+                }}
+                color={currentRating <= rating ? "orange" : "grey"}
+              />
             );
           })}
         </div>
+        <textarea
+          value={review}
+          onChange={(event) => setReview(event.target.value)}
+          maxLength={250}
+          placeholder="Write your review here.."
+        />
+        <button>Submit</button>
+        <button onClick={toggleAddReview}>Exit</button>
       </div>
     </div>
   );
