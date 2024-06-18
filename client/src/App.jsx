be2e387759d8b7a72e405d73cebd1ef0c7eedccf
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-06-18 11:24:10 +0530

    Convert css to module css in Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
                   )}
                   <CustomerReviews />
                   <AboutUs />
-                  <Footer/>
+                  {/* <Footer /> */}
                 </>
               }
             />
