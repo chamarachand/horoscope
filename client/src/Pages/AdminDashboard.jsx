a223b36e8294d6b3b7c8de934d60cec435215055
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-06-20 14:09:36 +0530

    Create a basic admin navbar

--- client/src/Pages/AdminDashboard.jsx
+++ client/src/Pages/AdminDashboard.jsx
@@ -1,24 +1,29 @@
+import { AdminNavBar } from "./AdminNavbar";
+
 export const AdminDashboard = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <h1>Welcome Admin</h1>
-      <p>This page is under construction</p>
-      <button
-        onClick={() => {
-          localStorage.removeItem("token");
-          window.location.reload();
+    <>
+      <AdminNavBar />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
         }}
       >
-        Logout
-      </button>
-    </div>
+        <h1>Welcome Admin</h1>
+        <p>This page is under construction</p>
+        <button
+          onClick={() => {
+            localStorage.removeItem("token");
+            window.location.reload();
+          }}
+        >
+          Logout
+        </button>
+      </div>
+    </>
   );
 };
---
+++ client/src/Pages/AdminNavbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export const AdminNavBar = () => {
+  return (
+    <nav className="">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/horoscopes">Hosroscopes</Link>
+        </li>
+        <li>
+          <Link to="/reviews">Reviews</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
