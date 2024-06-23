289de874d63885356b8722e8480d860d714c9dd5
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-06-23 13:20:45 +0530

    Improve ui, create basic error handling for the review reject reason box

--- client/src/components/ConfirmDeleteBox.jsx
+++ client/src/components/ConfirmDeleteBox.jsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import styles from "../styles/ConfirmDeleteBox.module.css";
 
 export const ConfirmDeleteBox = ({ setShowConfirmDeleteBox }) => {
+  const [rejectReason, setRejectReason] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleContinueBtnClick = () => {
+    if (!rejectReason)
+      return setError("Please provide a reason for rejecting the review");
+
+    if (rejectReason?.length < 5)
+      return setError("Please provide a valid and descriptive reason");
+  };
+
   return (
     <div className={styles.overlay}>
       <div className={styles.popupContainer}>
-        {/* <button
-          className={styles.crossBtn}
-          onClick={() => setShowConfirmDeleteBox(false)}
-        >
-          <img src="cross.png" alt="" />
-        </button> */}
-        <img src="cross.png" alt="" />
+        <div className={styles.crossBtnContainer}>
+          <button
+            className={styles.crossBtn}
+            onClick={() => setShowConfirmDeleteBox(false)}
+          >
+            <img src="/cross.png" alt="" />
+          </button>
+        </div>
+
         <div>
           <div className={styles.inputContent}>
             <label htmlFor="textarea">
               Please enter the reason for rejecting the review
             </label>
-            <textarea className={styles.textarea} id="textarea"></textarea>
+            <textarea
+              className={styles.textarea}
+              id="textarea"
+              onChange={(event) => setRejectReason(event.target.value.trim())}
+            ></textarea>
+            <p className={styles.error}>{error}</p>
           </div>
           <div className={styles.btnContainer}>
             <button
@@ -25,7 +44,12 @@ export const ConfirmDeleteBox = ({ setShowConfirmDeleteBox }) => {
             >
               Cancel
             </button>
-            <button className={styles.continueBtn}>Continue</button>
+            <button
+              className={styles.continueBtn}
+              onClick={handleContinueBtnClick}
+            >
+              Continue
+            </button>
           </div>
         </div>
       </div>
